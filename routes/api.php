28bb20990d61e1c0d042ea417720ff8d18f8d2ce
Theo <125301015+MattManamtam@users.noreply.github.com>
2024-08-24 07:19:03 +0800

    Update duties api

--- routes/api.php
+++ routes/api.php
@@ -41,6 +41,7 @@ Route::post('/students/duties/{dutyId}/request', [StudentDutyController::class, 
 Route::get('/students/duties/requested', [StudentDutyController::class, 'viewRequestedDuties']);
 Route::get('/students/duties/{dutyId}/details', [StudentDutyController::class, 'viewRequestedDutyDetails']);
 Route::delete('/students/duties/{dutyId}/cancel', [StudentDutyController::class, 'cancelRequest']);
+Route::get('/students/duties/accepted', [StudentDutyController::class, 'viewAcceptedDuties']);
 Route::get('/students/duties/completed', [StudentDutyController::class, 'viewCompletedDuties']);
     });
 
@@ -57,16 +58,41 @@ Route::middleware(['auth:sanctum', isProfessor::class])->group(function (){
 
 //Prof nirerate si Student.
 Route::post('/feedback/{student_id}', [StudentFeedbackController::class, 'store']);    
+
 //PROFESSOR DUTY APIS
+//Create a new duty - (new created duties default value set to duty_status = pending)
 Route::post('/professors/duties/create', [DutyProfController::class, 'create']);
-    Route::get('/professors/duties', [DutyProfController::class, 'index']);
-    Route::get('/professors/duties/{dutyId}', [DutyProfController::class, 'show']);
-    Route::put('/professors/duties/{dutyId}', [DutyProfController::class, 'update']);
-    Route::delete('/professors/duties/{dutyId}', [DutyProfController::class, 'delete']);
-    Route::post('/professors/duties/{dutyId}/accept/{studentId}', [DutyProfController::class, 'acceptStudent']);
-    Route::post('/professors/duties/{dutyId}/reject/{studentId}', [DutyProfController::class, 'rejectStudent']);
-    Route::put('/professors/duties/{dutyId}/status', [DutyProfController::class, 'updateStatus']);
-    Route::post('/professors/duties/{dutyId}/lock', [DutyProfController::class, 'lockDuty']);
+
+//Get function for the prof to display necessary info for his duties
+Route::get('/professors/duties', [DutyProfController::class, 'index']);
+
+//Get function for prof to get details of one specific duty
+Route::get('/professors/duties/{dutyId}', [DutyProfController::class, 'show']);
+
+// Get function para makuha kung ilang numbers of request and names ng mga nagrequest sa duty ng Prof
+Route::get('/professors/duties/{dutyId}/requests', [DutyProfController::class, 'getRequestsForDuty']);
+
+// Put function for the prof to update duties info as long as there's no accepted request 
+Route::put('/professors/updateInfo/{dutyId}', [DutyProfController::class, 'update']);
+
+// Delete function for a duty na wala pang accepted request si prof 
+Route::delete('/professors/duties/{dutyId}', [DutyProfController::class, 'delete']);
+
+// For accepting and rejecting a request for a duty from students
+Route::post('/professors/duties/{dutyId}/accept/{studentId}', [DutyProfController::class, 'acceptStudent']);
+Route::post('/professors/duties/{dutyId}/reject/{studentId}', [DutyProfController::class, 'rejectStudent']);
+
+//Show status of duties with accepted students
+Route::get('/professors/duties/{dutyId}/accepted-students', [DutyProfController::class, 'getAcceptedStudents']);
+
+// Prof can only update the duty_status("Pending, Active, Completed");
+    Route::patch('/duties/{dutyId}/status', [DutyProfController::class, 'updateStatus']);
+   
+// Cancel a duty (duty_status = cancelled) only once
+Route::delete('/duties/{dutyId}/cancel', [DutyProfController::class, 'cancelDuty']);
+
+//Simple function to lock duty - no purpose
+Route::post('/professors/duties/{dutyId}/lock', [DutyProfController::class, 'lockDuty']);
 
 
 
