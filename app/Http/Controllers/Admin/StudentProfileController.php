de7c52fbb07c3cae589d790f16297f97c6d435b7
Paul Vincent Sumalinog <sumalinog1124@gmail.com>
2024-09-07 20:57:30 +0800

    .

--- app/Http/Controllers/Admin/StudentProfileController.php
+++ app/Http/Controllers/Admin/StudentProfileController.php
@@ -3,9 +3,40 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class StudentProfileController extends Controller
 {
-    //
+
+    public function index(){
+        return view('students.student_add');
+    }
+
+    public function register(Request $request){
+        $fields = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|unique:users,email',
+            'password' => 'required|string|confirmed',
+
+        ]);
+
+        User::create([
+            'name' => $fields['name'],
+            'email' => $fields['email'],
+            'password' => bcrypt($fields['password']),
+        ]);
+
+        return redirect()->route('students.hkDutyQuota');
+
+    }
+
+
+    public function hkQuotaIndex(){
+        return view('students.hkDutyQuota');
+    }
+
+    public function hkQuotaStore(){
+
+    }
 }
