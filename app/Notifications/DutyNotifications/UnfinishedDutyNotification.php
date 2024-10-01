8f6056974f18ed80057da662d06462704a0e9960
MattManamtam <manamtammatttheodore@gmail.com>
2024-10-01 21:05:26 +0800

    Need test

--- app/Notifications/DutyNotifications/UnfinishedDutyNotification.php
+++ app/Notifications/DutyNotifications/UnfinishedDutyNotification.php
@@ -1,11 +1,12 @@
 <?php
-namespace App\Notifications;
+
+namespace App\Notifications\DutyNotifications;
 
 use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Notification;
 use Illuminate\Notifications\Messages\BroadcastMessage;
 
-class CancelledDutyNotification extends Notification
+class UnfinishedDutyNotification extends Notification
 {
     use Queueable;
 
@@ -24,8 +25,8 @@ class CancelledDutyNotification extends Notification
     public function toDatabase($notifiable)
     {
         return [
-            'title' => 'Duty Cancelled!',
-            'message' => 'The duty at ' . $this->duty->building . ' on ' . $this->duty->date . ' has been cancelled.',
+            'title' => 'Unfinished Duty!',
+            'message' => 'The duty scheduled for ' . $this->duty->date . ' was not completed.',
             'duty_id' => $this->duty->id,
             'time' => now(),
         ];
@@ -34,11 +35,10 @@ class CancelledDutyNotification extends Notification
     public function toBroadcast($notifiable)
     {
         return new BroadcastMessage([
-            'title' => 'Duty Cancelled!',
-            'message' => 'The duty at ' . $this->duty->building . ' on ' . $this->duty->date . ' has been cancelled.',
+            'title' => 'Unfinished Duty!',
+            'message' => 'The duty scheduled for ' . $this->duty->date . ' was not completed.',
             'duty_id' => $this->duty->id,
             'time' => now(),
         ]);
     }
 }
-
