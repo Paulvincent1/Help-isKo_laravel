30d873e29ef0b5d256fe72d277d012e5afcfcee5
John Brandon Lambino <johnbrandonlambino123@gmail.com>
2024-10-28 15:43:24 +0800

    adadad

--- app/Http/Controllers/Duty/EmployeeDutyController.php
+++ app/Http/Controllers/Duty/EmployeeDutyController.php
@@ -119,11 +119,6 @@ class EmployeeDutyController extends Controller
                 ->with('student.studentProfile')
                 ->get()
                 ->map(function ($record) {
-                    $activeDutiesCount =  $record->student->StudentDutyRecord()->whereHas('duty', function ($query) {
-                        $query->where('is_locked', true)
-                            ->where('duty_status', 'active');
-                    })
-                    ->count();
                     $activeDutiesCount = StudentDutyRecord::where('stud_id', $record->student->id)
                         ->whereHas('duty', function ($query) {
                             $query->where('is_locked', true)
